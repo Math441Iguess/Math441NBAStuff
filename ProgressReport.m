7cae298a39e924a57769431ea7fd6306b502888a
Math441Iguess <44657173+Math441Iguess@users.noreply.github.com>
2018-11-18 15:38:14 -0800

    This is the most updated file. Please do not delete

--- ProgressReport.m
+++ ProgressReport.m
@@ -88,17 +88,18 @@ TTS = data(:,18);
 clearvars data raw;
 
 
-
-f = [ Salary']/1000000
-A = -[-MINUTES PTS AST TRB STL BLK FGA FGM FTA FTM TPA TPM PACE/13 -PF ORB DRB -TOV]'
-b =-[-MP; TPTS; TAST; TTRB; TSTL; TBLK; TFGA; TFGM; TFTA; TFTM; TTPA; TTPM; TPACE; -TPF; TORB; TDRB; -TTOV]
+PLAYERS = [ones(1:282)]
+f = [Salary']/1000000;
+A = -[ PLAYERS PLAYERS -MINUTES PTS AST TRB STL BLK FGA FGM FTA FTM TPA TPM PACE/13 -PF ORB DRB -TOV]'
+b = -[ 13; -15; -MP; TPTS; TAST; TTRB; TSTL; TBLK; TFGA; TFGM; TFTA; TFTM; T3PA; T3PM; TPACE; -TPF; TORB; TDRB; -TTOV]
 Aeq = []
 beq = []
 lb = [zeros(1,282)]
 ub = [ones(1,282)]
 intcon = [1:282]
 
+
 [X,Z] = intlinprog(f,intcon,A,b,Aeq,beq,lb,ub)
 
-
+Y = nnz(X)
 
